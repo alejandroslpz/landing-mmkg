61a1bdc1c53c14c6cb8d222d9d465ab18a9cde20
Alejandro Sánchez López <alejandrosanchezlopez@MacBook-Air-de-Alejandro.local>
2020-10-31 14:34:53 -0600

    Fix bug deploy

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,6 @@ import styled from "@emotion/styled";
 import Nav from "./Nav";
 import ImagenHome from "../../assets/img/home.jpg";
 import { Parallax } from "react-scroll-parallax";
-import Button from "./Button";
 
 const Home = styled.div`
   width: 100vw;
