d462865e604922a1d4779ef9d735a6bd8079c8c9
Alejandro Sánchez López <alejandrosanchezlopez@MacBook-Air-de-Alejandro.local>
2020-10-31 09:19:11 -0600

    Actualización Layout principal App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { ParallaxProvider } from "react-scroll-parallax";
+import Registro from "./components/registro/Registro";
+import Bases from "./components/bases/Bases";
 
 const App = () => {
-  return ( 
-    <h1>Hola mundo</h1>
-   );
-}
- 
+  return (
+    <ParallaxProvider>
+      <Header />
+      <Registro />
+      <Bases />
+      <Footer />
+    </ParallaxProvider>
+  );
+};
+
 export default App;
