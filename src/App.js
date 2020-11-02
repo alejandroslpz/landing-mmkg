776bd03b4e06e2f17c194fbbb1e182f09a33a4a8
Alejandro Sánchez López <alejandrosanchezlopez@MacBook-Air-de-Alejandro.local>
2020-11-01 21:29:23 -0600

    Logica para nav de escritorio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/header/Header";
+import Nav from "./components/Nav";
 import Footer from "./components/footer/Footer";
 import { ParallaxProvider } from "react-scroll-parallax";
 import Registro from "./components/registro/Registro";
 import Bases from "./components/bases/Bases";
 
 const App = () => {
+  const [display, setDisplay] = useState("none");
+  const [seccion, setSeccion] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 700) {
+        setDisplay("flex");
+      } else {
+        setDisplay("none");
+      }
+    });
+  }, [display]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset < 700) {
+        setSeccion("Home");
+      } else if (window.pageYOffset < 1400) {
+        setSeccion("Registro");
+      } else if (window.pageYOffset < 2100) {
+        setSeccion("Bases");
+      } else if (window.pageYOffset < 2800) {
+        setSeccion("Premio");
+      }
+    });
+  }, [seccion]);
+
   return (
     <ParallaxProvider>
+      <Nav display={display} seccion={seccion} />
       <Header />
       <Registro />
       <Bases />
